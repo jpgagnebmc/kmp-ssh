6a486c67d03915c153bea150a24651d8b4a6982e
jp <jp@affair.dev>
2021-04-30 12:11:23 -0400

    disabled main

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,24 +10,24 @@ import kotlinx.serialization.json.Json
 data class TestSshConfig(val username: String, val password: String, val hostname: String)
 
 fun main(args: Array<String>) {
-    println("MAIN!!!!!!!!!!")
-
-    runBlockingCommon {
-        val config =
-            Json.decodeFromString(TestSshConfig.serializer(), File("~/.affair/TestSshConfig.json").readText())
-        println("helloWorld START $config")
-
-        val client = com.baremetalcloud.kmpssh.Ssh(
-            host = com.baremetalcloud.kmpssh.SshHostImpl(hostname = config.hostname),
-            identity = SshIdentity.Password(username = config.username, password = config.password),
-            options = SshOptionsImpl(),
-        )
-        println(client)
-        client.exec("echo 'hello world'").apply {
-            println("exec returned $out")
-//            assertTrue(out.trim() == "hello world")
-        }
-        println("helloWorld END")
-    }
+//    println("MAIN!!!!!!!!!!")
+//
+//    runBlockingCommon {
+//        val config =
+//            Json.decodeFromString(TestSshConfig.serializer(), File("~/.affair/TestSshConfig.json").readText())
+//        println("helloWorld START $config")
+//
+//        val client = com.baremetalcloud.kmpssh.Ssh(
+//            host = com.baremetalcloud.kmpssh.SshHostImpl(hostname = config.hostname),
+//            identity = SshIdentity.Password(username = config.username, password = config.password),
+//            options = SshOptionsImpl(),
+//        )
+//        println(client)
+//        client.exec("echo 'hello world'").apply {
+//            println("exec returned $out")
+////            assertTrue(out.trim() == "hello world")
+//        }
+//        println("helloWorld END")
+//    }
 
 }
